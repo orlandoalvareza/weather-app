95031d0472ef5c0e4f7be9110f47ee1e0c4dfefc
Orlando Alvarez <oalvarezalarcon0304@gmail.com>
2023-11-22 20:38:04 -0500

    added url for weather api

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import Header from "./components/Header";
+
 function App() {
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=${process.env.REACT_APP_API_KEY}`;
+  
   return (
     <div className="App">
-      
+      <Header/>
+      <form>
+        <label htmlFor="city">Search for a city</label>
+        <input type="text" id="city" name="city"/>
+        <button type="submit">Search</button>
+      </form>
+      <div className="temperature-container">
+        28 *C
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+const Header: React.FC = () => {
+  return (
+    <header>
+      <h1>Weather</h1>
+      <button>Temp</button>
+    </header>
+  )
+}
+
+export default Header;
