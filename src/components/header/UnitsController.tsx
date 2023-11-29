eafe5e816628b626a150fd02c77c54be226f13d4
Orlando Alvarez <oalvarezalarcon0304@gmail.com>
2023-11-29 01:45:00 -0500

    added somes styles to units controller component

--- src/components/header/UnitsController.tsx
+++ src/components/header/UnitsController.tsx
@@ -1,11 +1,26 @@
+import { useState } from 'react';
+
 import modules from './UnitsController.module.css';
 
 const UnitsController = () => {
+  const [isCelsius, setIsCelsius] = useState(false);
+
+  const TemperatureUnitHandler = () => {
+    setIsCelsius(isCelsius => !isCelsius)
+  }
+
+  const unitStyle = (
+    isCelsius ? "units-indicator__celsius" : "units-indicator__fahrenheit"
+  )
+
   return (
     <div className={modules["units-controller"]}>
       <span>°F</span>
-      <div className={modules["units-controller__indicator"]}>
-        <button>Click</button>
+      <div className={modules["units-indicator"]}>
+        <button 
+          onClick={TemperatureUnitHandler}
+          className={modules[unitStyle]}
+          ></button>
       </div>
       <span>°C</span>
     </div>
