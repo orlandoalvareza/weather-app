8cba77911af56960e394fae28c0d527d4c8189d1
Orlando Alvarez <oalvarezalarcon0304@gmail.com>
2024-02-05 23:37:55 -0500

    replaced form in error component and reused city input component

--- src/components/error/Error.tsx
+++ src/components/error/Error.tsx
@@ -1,20 +1,16 @@
+import CityInput from "../header/CityInput";
+
+import modules from './Error.module.css';
+
 const Error = () => {
   return (
-    <div>
-      <div>
+    <div className={modules["error-container"]}>
+      <div className={modules["error-information"]}>
         <h1>An error has occurred</h1>
         <h3>The city entered has not been found</h3>
         <p>Please try again or enter a new city</p>
       </div>
-      <form>
-        <input 
-          type="text" 
-          placeholder="Search for a city"
-        />
-        <button type="submit">
-          Search
-        </button>
-      </form>
+      <CityInput/>
     </div>
   )
 }
